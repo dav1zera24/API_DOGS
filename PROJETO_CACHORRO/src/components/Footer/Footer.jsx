71dcca8dbce468251703cc49cd41ff370f7b76b5
gustavobonetto-droid <gustavo.bonetto@aluno.senai.br>
2026-04-14 15:49:24 -0300

    all update

--- PROJETO_CACHORRO/src/components/Footer/Footer.jsx
+++ PROJETO_CACHORRO/src/components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
-const Header = () => {
-  return (
-    <header>
-      <h1>Dog Explorer</h1>
-      <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px 0' }}>
-        <h3>Entenda o Modelo Cliente-Servidor:</h3>
-        <p>
-          O <strong>Cliente</strong> (seu navegador) faz uma requisição HTTP para o 
-          <strong> Servidor</strong> (API Dog CEO). O servidor processa e envia de volta 
-          uma resposta JSON com a imagem do cachorro.
-        </p>
-      </div>
-    </header>
-  );
-};
+import './Footer.css';
 
-export default Header;
+function Footer() {
+    return (
+        <>
+            <footer className="footer">
+              <div>
+                    <p>© 2026 DOGS EXPLORER.</p>
+                    <p>Desenvolvido por Men's Caves</p>
+                </div>
+               
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
--- PROJETO_CACHORRO/src/components/Header/Header.jsx
+++ PROJETO_CACHORRO/src/components/Header/Header.jsx
@@ -3,15 +3,7 @@ import './Header.css'
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-title">Dog Explorer</h1>
-      <div className="header-box">
-        <h3>Entenda o Modelo Cliente-Servidor:</h3>
-        <p>
-          O <strong>Cliente</strong> (seu navegador) faz uma requisição HTTP para o
-          <strong> Servidor</strong> (API Dog CEO). O servidor processa e envia de volta
-          uma resposta JSON com a imagem do cachorro.
-        </p>
-      </div>
+      <h1 className="header-title">DOG EXPLORER</h1>
     </header>
   )
 }
